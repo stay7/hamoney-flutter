69560b6b9c6524be4a35ce2a6822ef626fa88ecb
stay7 <codelatte20@gmail.com>
2023-04-23 23:22:57 +0900

    탭 리소스 등록

--- lib/resource/app_image.dart
+++ lib/resource/app_image.dart
@@ -4,4 +4,15 @@ class AppImage {
   static const loginGoogle = "assets/login_google.svg";
   static const loginApple = "assets/login_apple.svg";
   static const loginKakao = "assets/login_kakao.svg";
+
+  static const tabIconCalendarOff = "assets/tab_icon_calendar_off.svg";
+  static const tabIconCalendarOn = "assets/tab_icon_calendar_on.svg";
+  static const tabIconPlusOff = "assets/tab_icon_plus_off.svg";
+  static const tabIconPlusOn = "assets/tab_icon_plus_on.svg";
+  static const tabIconSavingOff = "assets/tab_icon_saving_off.svg";
+  static const tabIconSavingOn = "assets/tab_icon_saving_on.svg";
+  static const tabIconSettingOff = "assets/tab_icon_setting_off.svg";
+  static const tabIconSettingOn = "assets/tab_icon_setting_on.svg";
+  static const tabIconPlanOff = "assets/tab_icon_plan_off.svg";
+  static const tabIconPlanOn = "assets/tab_icon_plan_on.svg";
 }
